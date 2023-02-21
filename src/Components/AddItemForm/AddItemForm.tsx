bf1d16ded5fa0704a59b4b17fa8a33396d2c1ae6
zanzipanziby <zanzipanziby@gmail.com>
2023-02-21 22:10:43 +0300

    refactoring

--- src/Components/AddItemForm/AddItemForm.tsx
+++ src/Components/AddItemForm/AddItemForm.tsx
@@ -1,69 +1,1 @@
-import React, {ChangeEvent, useState} from 'react';
-import s from "./AddItemForm.module.css"
-import {TextField, Box, Button, Icon} from "@material-ui/core";
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import {ResponseStatusType} from "../../Types/Types";
-import AddIcon from '@material-ui/icons/Add';
-
-
-type AddItemFormPropsType = {
-    label: string
-    options?: ResponseStatusType
-    callback: (title: string) => void
-}
-export const AddItemForm = (props: AddItemFormPropsType) => {
-    const [title, setTitle] = useState('')
-    const [error, setError] = useState<string | null>(null)
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setError(null)
-        setTitle(e.currentTarget.value)
-    }
-    const onClick = () => {
-        if (title.trim()) {
-            props.callback(title.trim())
-            setTitle('')
-        } else {
-            setError('Please,enter value')
-        }
-
-    }
-
-    return (
-        <>
-            <Box className={s.addItemFormWrapper}>
-                <TextField
-                    label={!error ? props.label : "Please, enter value"}
-                    error={!!error}
-                    size={'medium'}
-                    variant="outlined"
-                    value={title}
-                    onChange={onChange}
-                    onKeyDown={e => e.key === 'Enter' && onClick()}
-                    onBlur={() => setError(null)}
-                    className={s.textField}
-                    disabled={props.options === 'loading'}
-                />
-                <Box>
-                    {/*<AddCircleOutlineIcon*/}
-                    {/*    fontSize={'large'}*/}
-                    {/*    color={'primary'}*/}
-                    {/*    onClick={onClick}*/}
-                    {/*    aria-disabled={!!error && props.options === 'loading'}*/}
-                    {/*/>*/}
-                    <Button
-                        size={'medium'}
-                        variant={'text'}
-                        color={'primary'}
-                        disabled={!!error || props.options === 'loading'}
-                        onClick={onClick}
-                        fullWidth={false}
-                    >
-                        <AddIcon/>
-                    </Button>
-                </Box>
-            </Box>
-        </>
-
-    );
-}
-
+import React, {ChangeEvent, useState} from 'react';import s from "./AddItemForm.module.css"import {TextField, Box, Button, Icon} from "@material-ui/core";import {ResponseStatusType} from "../../Types/Types";import {AddBox} from "@material-ui/icons";type AddItemFormPropsType = {    label: string    options?: ResponseStatusType    callback: (title: string) => void}export const AddItemForm = (props: AddItemFormPropsType) => {    const [title, setTitle] = useState('')    const [error, setError] = useState<string | null>(null)    const onChange = (e: ChangeEvent<HTMLInputElement>) => {        setError(null)        setTitle(e.currentTarget.value)    }    const onClick = () => {        if (title.trim()) {            props.callback(title.trim())            setTitle('')        } else {            setError('Please,enter value')        }    }    return (        <>            <Box className={s.addItemFormWrapper}>                <TextField                    label={!error ? props.label : "Please, enter value"}                    error={!!error}                    size={'medium'}                    variant="outlined"                    value={title}                    onChange={onChange}                    onKeyDown={e => e.key === 'Enter' && onClick()}                    onBlur={() => setError(null)}                    className={s.textField}                    disabled={props.options === 'loading'}                />                <Box>                    <Button                        size={'large'}                        variant={'text'}                        color={'primary'}                        disabled={!!error || props.options === 'loading'}                        onClick={onClick}                    >                        <AddBox />                    </Button>                </Box>            </Box>        </>    );}
